3074175b27f5cc9f09e6e1bfc32093759f8bdbba
Thomas Scott <t.scott12331@gmail.com>
2025-06-16 00:33:39 -0700

    refactor: (merge with previous commit)

---
+++ lib/components/util/toggle-dropdown.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+import styles from './toggle-dropdown.module.css';
+
+interface ToggleDropdownProps<T> {
+    onToggle?: (state: boolean) => Promise<void> | void;
+    onSelect?: (item: T) => Promise<void> | void;
+    toString: (item: T) => string;
+    toId: (item: T) => string | number;
+    initialSelectId: string;
+    width?: string;
+    items: T[];
+    toggleOnRender?: React.ReactNode;
+    toggleOffRender?: React.ReactNode;
+}
+
+export default function ToggleDropdown<T>({
+    onToggle,
+    onSelect,
+    toString,
+    toId,
+    initialSelectId,
+    toggleOnRender,
+    toggleOffRender,
+    width = "20%",
+    items,
+}: ToggleDropdownProps<T>) {
+    const [shouldShowDd, setShouldShowDd] = useState<boolean>(false);
+    const [selItem, setSelItem] = useState<string|number>(initialSelectId);
+    const [isToggled, setIsToggled] = useState<boolean>(false);
+
+    const onGlobalClick = () => {
+        if(shouldShowDd) setShouldShowDd(false);
+    }
+
+    const handleDropdownSelect = (item: T) => {
+        setSelItem(toId(item));
+        onSelect?.(item);
+    }
+
+    const toggle = () => {
+        onToggle?.(!isToggled);
+        setIsToggled(!isToggled);
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', onGlobalClick);
+
+        return () => document.removeEventListener('click', onGlobalClick);
+    }, [shouldShowDd])
+
+
+    return (
+        <div className={styles.wrapper}
+            style={{
+                width: `min(80px, max(${width}, 50px))`,
+                paddingBottom: `min(80px, max(${width}, 50px))`,
+            }}
+        >
+            <button className={styles.toggleButton}
+            onClick={toggle}
+            >
+                {isToggled ?
+                    toggleOffRender ?? "off"
+                :
+                    toggleOnRender ?? "on"
+                }
+            </button>
+            <div className={styles.dropdownToggle}
+            onClick={() => setShouldShowDd(!shouldShowDd)}
+            ></div>
+            { shouldShowDd &&
+            <div className={styles.dropdownWrapper}>
+                {
+                    items.length === 0 ?
+                    <div className={styles.dropdownItem}>No items</div>
+                    :
+                    items.map(item => 
+                    <div className={styles.dropdownItem}
+                        data-selected={selItem === toId(item) ? "true" : "false"}
+                        onClick={() => handleDropdownSelect(item)}
+                        key={toId(item)}
+                    >{toString(item)}</div>
+                    )
+                }
+            </div>
+            }
+        </div>
+    )
+}
