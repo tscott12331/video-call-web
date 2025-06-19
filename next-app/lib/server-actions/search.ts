b51dbb0327ce0cdf03339acc084bc0740ca02841
Thomas Scott <t.scott12331@gmail.com>
2025-06-19 15:50:57 -0700

    feat: filter out self when searching users

--- next-app/lib/server-actions/search.ts
+++ next-app/lib/server-actions/search.ts
@@ -3,6 +3,8 @@
 import { ilike } from "drizzle-orm";
 import { db } from "../db/db";
 import { UserProfileTable } from "../db/schemas/user";
+import { cookies } from "next/headers";
+import { verifyJWT } from "../auth/jwt";
 
 export async function searchUsers(phrase: string, limit: number = 10, offset: number = 0) {
     try {
@@ -12,6 +14,15 @@ export async function searchUsers(phrase: string, limit: number = 10, offset: nu
         .where(ilike(UserProfileTable.Username, `%${phrase}%`))
         .limit(limit)
         .offset(offset);
+
+        const token = (await cookies()).get('token');
+
+        if(token) {
+            const tokenUsername = await verifyJWT(token.value);
+            if(!!tokenUsername) {
+                return userList.filter((user) => user.username !== tokenUsername.payload.username)
+            }
+        }
 
         return userList;
     } catch(err) {
