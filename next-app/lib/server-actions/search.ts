eabaf093243f28c5f9d3314c9c746920623a8420
Thomas Scott <t.scott12331@gmail.com>
2025-06-19 17:42:16 -0700

    feat: search for friends returns friendship status

--- next-app/lib/server-actions/search.ts
+++ next-app/lib/server-actions/search.ts
@@ -1,30 +1,41 @@
 "use server";
 
-import { ilike } from "drizzle-orm";
+import { and, eq, ilike, not } from "drizzle-orm";
 import { db } from "../db/db";
-import { UserProfileTable } from "../db/schemas/user";
+import { UserFriendTable, UserProfileTable } from "../db/schemas/user";
 import { cookies } from "next/headers";
 import { verifyJWT } from "../auth/jwt";
 
 export async function searchUsers(phrase: string, limit: number = 10, offset: number = 0) {
     try {
+        const token = (await cookies()).get('token')?.value;
+        if(!token) return [];
+
+        const tokenValue = await verifyJWT(token);
+        if(!tokenValue) return [];
+
+        const username = tokenValue.payload.username as string|undefined;
+        if(!username) return [];
+
         const userList = await db.select({
-            username: UserProfileTable.Username
+            username: UserProfileTable.Username,
+            requestIsAccepted: UserFriendTable.IsAccepted,
         }).from(UserProfileTable)
-        .where(ilike(UserProfileTable.Username, `%${phrase}%`))
-        .limit(limit)
-        .offset(offset);
+        .leftJoin(UserFriendTable,
+                  and(
+                      eq(UserFriendTable.UserProfile_Username, username),
+                      eq(UserProfileTable.Username, UserFriendTable.AddedProfile_Username))
+                 )
+                 .where(
+                     and(
+                         not(eq(UserProfileTable.Username, username)),
+                         ilike(UserProfileTable.Username, `%${phrase}%`))
+                 )
+                 .limit(limit)
+                 .offset(offset);
 
-        const token = (await cookies()).get('token');
-
-        if(token) {
-            const tokenUsername = await verifyJWT(token.value);
-            if(!!tokenUsername) {
-                return userList.filter((user) => user.username !== tokenUsername.payload.username)
-            }
-        }
-
-        return userList;
+         return userList;
+         //return userList.filter((user) => user.username !== username)
     } catch(err) {
         console.error(err);
         return [];
