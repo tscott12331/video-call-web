564452070c59004502c58c666453823a1205ea96
Thomas Scott <t.scott12331@gmail.com>
2025-06-24 15:35:18 -0700

    feat: sidebar lists added friends

--- next-app/lib/components/sidebar/sidebar.tsx
+++ next-app/lib/components/sidebar/sidebar.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import styles from "./sidebar.module.css";
+import { getFriends } from "@/lib/server-actions/friend";
 
 interface SidebarProps {
     onAddFriendClick?: () => void;
 }
 
+type friend = {
+    username: string;
+}
+
 export default function Sidebar({
     onAddFriendClick,
 }: SidebarProps) {
+    const [friendList, setFriendList] = useState<friend[]>([]);
+
+    const initFriendList = async () => {
+        const list = await getFriends();
+        if(list.success) {
+            setFriendList(list.friendList);
+        }
+    }
+
+    useEffect(() => {
+        initFriendList();
+    }, [])
+
     return (
         <section className={styles.sbSection}>
             <div className={styles.sbCtrls}>
@@ -23,6 +42,24 @@ export default function Sidebar({
                     <button className={styles.sbSearchBut}>search</button>
                 </div>
             </div>
-            <div className={styles.sbFriendsList}></div>
+            <div className={styles.sbFriendsList}>
+            {
+                friendList.map(friend =>
+                    <div 
+                    className={styles.sbFriend}
+                    key={friend.username}
+                    >
+                        <div className={styles.sbFriendLeft}>
+                            <div className={styles.pfpSmallWrapper}></div>
+                            <p className={styles.sbFriendName}>{friend.username}</p>
+                        </div>
+                        <div className={styles.sbFriendRight}>
+                            <div className={styles.messageSmallIcon}></div>
+                            <div className={styles.videoSmallIcon}></div>
+                        </div>
+                    </div>
+                              )
+            }
+            </div>
         </section>);
 }
--- next-app/lib/server-actions/friend.ts
+++ next-app/lib/server-actions/friend.ts
@@ -2,9 +2,9 @@
 
 import { cookies } from "next/headers";
 import { db } from "../db/db";
-import { UserFriendTable } from "../db/schemas/user";
+import { UserFriendTable, UserProfileTable } from "../db/schemas/user";
 import { verifyJWT } from "../auth/jwt";
-import { and, eq, or } from "drizzle-orm";
+import { and, eq, not, or } from "drizzle-orm";
 import { authenticateUser } from "./auth";
 
 export async function friendAction(friendUsername: string) {
@@ -98,4 +98,45 @@ export async function acceptFriend(friendUsername: string) {
 
 export async function removeFriend(friendUsername: string) {
 
+}
+
+export async function getFriends(limit: number = 10, offset: number = 0) {
+    try {
+        const authRes = await authenticateUser();
+        if(!authRes.success) return { error: authRes.error };
+
+        const { username } = authRes;
+
+        const friendList = await db.select({
+            username: UserProfileTable.Username,
+        })
+            .from(UserProfileTable)
+            .innerJoin(UserFriendTable,
+                        or(
+                            eq(UserProfileTable.Username, UserFriendTable.UserProfile_Username),
+                            eq(UserProfileTable.Username, UserFriendTable.AddedProfile_Username)
+                        )
+                      )
+            .where(
+                and(
+                    or(
+                        eq(UserFriendTable.UserProfile_Username, username),
+                        eq(UserFriendTable.AddedProfile_Username, username)
+                    )
+                    ,
+                    eq(UserFriendTable.IsAccepted, true),
+                    not(eq(UserProfileTable.Username, username))
+                )
+            )
+            .limit(limit)
+            .offset(offset);
+
+            return {
+                success: true,
+                friendList
+            }
+    } catch(err) {
+        console.error(err);
+        return { error: "Server error" };
+    }
 }
